38785e183a6f8cffb8047ff3d2fe14fc9946e42f
Ayoub Dridi <ayoub.dridi2006@gmail.com>
2024-01-04 11:35:46 +0100

    Aggiungere i file di progetto.

---
+++ 13- aliante/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13__aliante
+{
+    class Componente
+    {
+
+    }
+
+    class Aliante
+    {
+
+    }
+
+    class Ala
+    {
+
+    }
+    class Fusioliera
+    {
+
+    }
+    class Coda
+    {
+
+    }
+    class Ruota
+    {
+
+    }
+
+}
---
+++ 13- aliante/Form1.cs
@@ -0,0 +1,12 @@
+namespace _13__aliante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
